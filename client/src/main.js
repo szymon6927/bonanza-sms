646f8ab46f6e749abe23e3ee4843d9e0028a5be1
Szymon Miks <miks.szymon@gmail.com>
2018-06-26 23:26:58 +0200

    add own css

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/style.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
