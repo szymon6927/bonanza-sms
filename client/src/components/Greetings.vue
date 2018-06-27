8bb64bf4bc80838dd955acc35ea6aaa6410725ae
Szymon Miks <miks.szymon@gmail.com>
2018-06-27 23:30:26 +0200

    add logo, home component and greetings compoent which is displayed after completed the form

---
+++ client/src/components/Greetings.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Dziękujemy za rejestrację i życzymy udanego wypoczynku</h1>
+    <v-btn color="success" to="/">Home</v-btn>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Greetings"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid class="mb-5">
+    <v-layout column align-center justify-center>
+      <div class="content__wraper text-xs-center pt-5 pb-5">
+        <img class="logo" src="/static/western.png">
+        <h1 class="black--text headline mb-3">Łap okazje i rabaty!</h1>
+        <h1 class="black--text display-1 bonanza-name we mb-3">Bar Bonanza!</h1>
+        <h3 class="black--text mb-4">Bądź z nami na bieżąco!</h3>
+        <v-btn round large block color="primary" to="/form">Zapisz się</v-btn>
+      </div>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    data () {
+      return {
+        message: "Bar bonanza! Bądź na bieżąco!"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
