38f3470f74315f62e25d668d22458ea3fc6111cf
Szymon Miks <miks.szymon@gmail.com>
2018-06-30 12:16:32 +0200

    add new font Catamaran

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -3,8 +3,8 @@
     <v-layout column align-center justify-center>
       <div class="content__wraper elevation-10 text-xs-center pt-5 pb-5">
         <img class="logo" src="/static/western.png">
-        <h1 class="black--text headline mb-3">Łap okazje i rabaty!</h1>
-        <h1 class="black--text display-1 bonanza-name we mb-3">Bar Bonanza!</h1>
+        <h1 class="black--text mb-3">Łap okazje i rabaty!</h1>
+        <h2 class="black--text display-1 bonanza-name we mb-3">Bar Bonanza!</h2>
         <h3 class="black--text mb-4">Bądź z nami na bieżąco!</h3>
         <v-btn round large block color="light-blue darken-4 white--text" to="/form">Zapisz się</v-btn>
       </div>
@@ -24,6 +24,9 @@
 </script>
 
 <style scoped>
+  h3 {
+    font-weight: 600;
+  }
   @media (min-width: 615px) {
     .logo {
       width: 40%;
